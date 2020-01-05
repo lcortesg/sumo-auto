e4685ff9d102c417641300d345bf10ea6b16e0fb
Lucas Felipe Cortés Gutiérrez <lcortes@alumnos.inf.utfsm.cl>
2020-01-04 23:52:46 -0300

    test

--- sumo-auto.ino
+++ sumo-auto.ino
@@ -1,88 +1,34 @@
 /**
- * @brief: Firmware developed for the Robot Sumo Auto named "Don Fede". It finds it's enemy using a PID controller.
+ * @brief: Firmware desarrollado para el Robot Sumo Don Fede que por ahora solo encuentra al rival usando un control PID.
  * @author : Diego Pandolfa.
  * @date: 12/10/2018
- * @translation: by Lucas Cortés.
  */
-
 #include <SoftwareSerial.h>
 #include <Sabertooth.h>
 
-#define NONE 0
-#define INFO 1
-#define SERIAL INFO
-#define DEBUG INFO
-
-#define PIN_SENSOR_LEFT 8
-#define PIN_SENSOR_F_LEFT 11
-#define PIN_SENSOR_FRONT 10
-#define PIN_SENSOR_F_RIGHT 9
-#define PIN_SENSOR_RIGHT 12
+#define PIN_SENSOR_LEFT 7
+#define PIN_SENSOR_F_LEFT 10
+#define PIN_SENSOR_FRONT 9
+#define PIN_SENSOR_F_RIGHT 8
+#define PIN_SENSOR_RIGHT 11
 
 #define PIN_LINE_F_LEFT A0
-#define PIN_LINE_F_RIGHT A2
-#define PIN_LINE_B_LEFT A1
+#define PIN_LINE_F_RIGHT A1
+#define PIN_LINE_B_LEFT A2
 #define PIN_LINE_B_RIGHT A3
 
-#define PIN_START_BUTTON 3
-#define PIN_DIP_SWITCH 2
-
-#define TIME_SURVIVE_1 120
-
-/**
- * @brief : Color threshold.
- * Under normal conditions white is measured under 100. 
- * under normal conditions black is measured over 700.
- * A safe threshold should be defined under test measurements done in the Dohyo.
- * 
- * @default 0 : It will not detect the white line.
- */
 #define TH_LINE 100
 
-/**
- * @brief : defines the maximum power given to the motors.
- */
 #define POWER_MAX 255
 
-/**
- * @brief : overflow defined for a loop cycle counter for debugging purposes.
- */
-#define TEST_COUNT_MAX 500
-
-/**
- * @brief : overflow defined for an enemy-not-found counter
- */
-#define MISS_COUNT_MAX 1000
-
-/*
 #define KP 0.018
 #define KI 0.0
 #define KD 0.008
-*/
-/*
-#define KP 0.008
-#define KI 0.0008
-#define KD 0.008
-*/
-/**
- * @brief : PID controller constants.
- * We used the Ziegler–Nichols method to tune this parameters.
- */
-#define KP 0.15 //0.064
-#define KI 0.0008
-#define KD 0.016
 
-/**
- * @brief: Reference value to follow with the PID controller.
- */
 #define REF 3000
 
-#define PIN_SERIAL_ST 6
 
-#define PIN_LED 13
-//#define PIN_LED 7
-
-SoftwareSerial SWSerial(NOT_A_PIN, PIN_SERIAL_ST);
+SoftwareSerial SWSerial(NOT_A_PIN, 2);
 Sabertooth ST(128, SWSerial); // Address 128, and use SWSerial as the serial port.
 
 double enemy_sensor_left = 0;
@@ -104,101 +50,99 @@ double error_dev = 0;
 double measure = 0;
 double ref = REF;
 
-int count = 0;
-int miss_count = 0;
-int tactic = 0;
-
-
 void setup(){
+  SWSerial.begin(9600);
+  ST.autobaud();
+  Serial.begin(115200);
+  pinMode(PIN_SENSOR_LEFT, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_F_LEFT, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_FRONT, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_F_RIGHT, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_RIGHT, INPUT_PULLUP);
 
-    SWSerial.begin(9600);
-    ST.autobaud();
-
-    #if SERIAL > NONE
-        Serial.begin(115200);
-        Serial.println("Init Sumo Robot - CoR 2019");
-    #endif
-
-    pinMode(PIN_SENSOR_LEFT, INPUT_PULLUP);
-    pinMode(PIN_SENSOR_F_LEFT, INPUT_PULLUP);
-    pinMode(PIN_SENSOR_FRONT, INPUT_PULLUP);
-    pinMode(PIN_SENSOR_F_RIGHT, INPUT_PULLUP);
-    pinMode(PIN_SENSOR_RIGHT, INPUT_PULLUP);
-
-    pinMode(PIN_LINE_F_LEFT, INPUT);
-    pinMode(PIN_LINE_F_RIGHT, INPUT);
-    pinMode(PIN_LINE_B_LEFT, INPUT);
-    pinMode(PIN_LINE_B_RIGHT, INPUT);
-
-    pinMode(PIN_LED, OUTPUT);
-    //digitalWrite(PIN_LED, LOW);
-
-    pinMode(PIN_DIP_SWITCH, INPUT_PULLUP);
-    tactic = digitalRead(PIN_DIP_SWITCH);
-
-    #if SERIAL > NONE
-        Serial.println("Wait for Button Start ...");
-    #endif
-
-    pinMode(PIN_START_BUTTON, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(PIN_START_BUTTON), stop, FALLING);
-    while(!digitalRead(PIN_START_BUTTON));
-    digitalWrite(PIN_LED, HIGH);
-
-    #if SERIAL > NONE
-        Serial.println("Wait for 5 seconds ... ");
-    #endif
-
-    delay(5050);
-
-    #if SERIAL > NONE
-        Serial.println("Fight!!!");
-    #endif
-    ST.motor(1, 255);
-    ST.motor(2, 255);
-    delay(250);
-    ST.motor(1, 0);
-    ST.motor(2, 0);
+  pinMode(PIN_LINE_F_LEFT, INPUT);
+  pinMode(PIN_LINE_F_RIGHT, INPUT);
+  pinMode(PIN_LINE_B_LEFT, INPUT);
+  pinMode(PIN_LINE_B_RIGHT, INPUT);
 }
 
 void loop(){
-    /*
-    count++; // Comment out this line for the final code
-  
-    if(measureLine()){
-        survive(); 
-    }
-    searchFWD();
-    */
+  line_sensor_f_left = analogRead(PIN_LINE_F_LEFT);
+  line_sensor_f_right = analogRead(PIN_LINE_F_RIGHT);
+  line_sensor_b_left = analogRead(PIN_LINE_B_LEFT);
+  line_sensor_b_right = analogRead(PIN_LINE_B_RIGHT);
+
+  Serial.print("\n>>> LOG: mediciones de linea: \t");
+  Serial.print(line_sensor_f_left);
+  Serial.print("\t");
+  Serial.print(line_sensor_f_right);
+  Serial.print("\t");
+  Serial.print(line_sensor_b_left);
+  Serial.print("\t");
+  Serial.print(line_sensor_b_right);
+
+  if( (line_sensor_f_left > TH_LINE) || (line_sensor_f_right > TH_LINE) || (line_sensor_b_left > TH_LINE) || (line_sensor_b_right > TH_LINE) ){
+    //implementar lógica de permanencia en el escenario aqui...   <----------------------------------------------------------------------------------------------
+      ST.motor(1, -POWER_MAX/4);
+      ST.motor(2, -POWER_MAX/4);
+  }
+  else{
+    enemy_sensor_left = !digitalRead(PIN_SENSOR_LEFT);
+    enemy_sensor_f_left = !digitalRead(PIN_SENSOR_F_LEFT);
+    enemy_sensor_front = !digitalRead(PIN_SENSOR_FRONT);
+    enemy_sensor_f_right = !digitalRead(PIN_SENSOR_F_RIGHT);
+    enemy_sensor_right = !digitalRead(PIN_SENSOR_RIGHT);
+
+    Serial.print("\n>>> LOG: mediciones de enemigo: \t");
+    Serial.print(enemy_sensor_left);
+    Serial.print("\t");
+    Serial.print(enemy_sensor_f_left);
+    Serial.print("\t");
+    Serial.print(enemy_sensor_front);
+    Serial.print("\t");
+    Serial.print(enemy_sensor_f_right);
+    Serial.print("\t");
+    Serial.print(enemy_sensor_right);
+
+    measure = ((enemy_sensor_left + enemy_sensor_f_left + enemy_sensor_front + enemy_sensor_f_right + enemy_sensor_right) == 0)?(2500):(enemy_sensor_left*1000 + enemy_sensor_f_left*2000 + enemy_sensor_front*3000 + enemy_sensor_f_right*4000 + enemy_sensor_right*5000)/(enemy_sensor_left + enemy_sensor_f_left + enemy_sensor_front + enemy_sensor_f_right + enemy_sensor_right);
+
+    Serial.print("\n>>> LOG: measure:\t");
+    Serial.println(measure);
     
-    if(measureEnemy()){
-        if(searchPID()){
-            fight();
-        }
-    }
-    else if(measureLine()){
-        survive(); 
-    }
+    error = measure - ref;
+    Serial.print("\n>>> LOG: error:\t");
+    Serial.println(error);
+    
+    error_int += (actuation >= POWER_MAX)?(0):(error);
+    Serial.print("\n>>> LOG: error int:\t");
+    Serial.println(error_int);
+    
+    error_dev = error - error_prev;
+    Serial.print("\n>>> LOG: error_dev:\t");
+    Serial.println(error_dev);
+    
+    error_prev = error;
+    Serial.print("\n>>> LOG: error_prev:\t");
+    Serial.println(error_prev);
 
-/*
-  #if DEBUG > NONE
-    Serial.print("\nCount Test:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\t");
-    Serial.println(count);
-  #endif
-  if(count >= TEST_COUNT_MAX){
-    ST.motor(1, 0);
-    ST.motor(2, 0);
-    while(1);
-  }*/ 
+    if(error == 0.0){
+      
+      // implementar lógica de combate aqui...  <-------------------------------------------------------------------------------------------------------------------
+      ST.motor(1, POWER_MAX/5);
+      ST.motor(2, POWER_MAX/5);
+    }
+    else{
+
+      // implementar lógica de búsqueda aquí...  <--------------------------------------------------------------------------------------------------------------------
+      actuation = (actuation >= POWER_MAX)?(POWER_MAX):(error*KP + error_int*KI + error_dev*KD);
+
+      Serial.print("\n>>> LOG: actuation:\t");
+      Serial.println(actuation);
+    
+      ST.motor(1, actuation);
+      ST.motor(2, -actuation);
+    }
+  }
+  delay(10);
 }
 
-
-void stop(){ 
-    ST.motor(1, 0);
-    ST.motor(2, 0);
-    digitalWrite(PIN_LED, LOW);
-    while(true){
-        ST.motor(1, 0);
-        ST.motor(2, 0);
-    }
-}
