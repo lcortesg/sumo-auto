5c494bfb0143748c8afedeec060a1c93dfe5135c
Lucas Felipe Cortés Gutiérrez <lcortes@alumnos.inf.utfsm.cl>
2020-01-05 00:06:58 -0300

    updated to v1.1.0

--- sumo-auto.ino
+++ sumo-auto.ino
@@ -1,7 +1,8 @@
 /**
- * @brief: Firmware desarrollado para el Robot Sumo Don Fede que por ahora solo encuentra al rival usando un control PID.
+ * @brief: Firmware developed for the Robot Sumo Auto named "Don Fede". It finds it's enemy using a PID controller.
  * @author : Diego Pandolfa.
- * @date: 04/12/2018
+ * @date: 12/10/2018
+ * @translation: by Lucas Cortés.
  */
 
 #include <SoftwareSerial.h>
@@ -9,7 +10,7 @@
 
 #define NONE 0
 #define INFO 1
-
+#define SERIAL INFO
 #define DEBUG INFO
 
 #define PIN_SENSOR_LEFT 8
@@ -19,36 +20,37 @@
 #define PIN_SENSOR_RIGHT 12
 
 #define PIN_LINE_F_LEFT A0
-#define PIN_LINE_F_RIGHT A1
-#define PIN_LINE_B_LEFT A2
+#define PIN_LINE_F_RIGHT A2
+#define PIN_LINE_B_LEFT A1
 #define PIN_LINE_B_RIGHT A3
 
-#define PIN_START_BUTTON 4
+#define PIN_START_BUTTON 3
 #define PIN_DIP_SWITCH 2
 
 #define TIME_SURVIVE_1 120
 
 /**
- * @brief : threshold de color.
- * normalmente mide bajo 100 en blanco y sobre 700 en negro
- * definir umbral seguro con desperfectos en el escenario.
+ * @brief : Color threshold.
+ * Under normal conditions white is measured under 100. 
+ * under normal conditions black is measured over 700.
+ * A safe threshold should be defined under test measurements done in the Dohyo.
  * 
- * @default 0 nunca detecta linea blanca
+ * @default 0 : It will not detect the white line.
  */
-#define TH_LINE 200
+#define TH_LINE 100
 
 /**
- * @brief : define la potencia máxima entregada a los motores
+ * @brief : defines the maximum power given to the motors.
  */
-#define POWER_MAX 100
+#define POWER_MAX 255
 
 /**
- * @brief : overflow definido para un contador de ciclos del loop para uso de testeo
+ * @brief : overflow defined for a loop cycle counter for debugging purposes.
  */
 #define TEST_COUNT_MAX 500
 
 /**
- * @brief : overflow definido para un contador de ciclos sin encontrar un enemigo
+ * @brief : overflow defined for an enemy-not-found counter
  */
 #define MISS_COUNT_MAX 1000
 
@@ -63,20 +65,22 @@
 #define KD 0.008
 */
 /**
- * @brief : constantes del controlador PID
+ * @brief : PID controller constants.
+ * We used the Ziegler–Nichols method to tune this parameters.
  */
-#define KP 0.032
+#define KP 0.15 //0.064
 #define KI 0.0008
 #define KD 0.016
 
 /**
- * @brief: referencia a seguir con el control PID
+ * @brief: Reference value to follow with the PID controller.
  */
 #define REF 3000
 
 #define PIN_SERIAL_ST 6
 
-#define PIN_LED 7
+#define PIN_LED 13
+//#define PIN_LED 7
 
 SoftwareSerial SWSerial(NOT_A_PIN, PIN_SERIAL_ST);
 Sabertooth ST(128, SWSerial); // Address 128, and use SWSerial as the serial port.
@@ -104,52 +108,78 @@ int count = 0;
 int miss_count = 0;
 int tactic = 0;
 
+
 void setup(){
-  SWSerial.begin(9600);
-  ST.autobaud();
-  Serial.begin(115200);
-  Serial.println("Init Sumo Robot - CoR 2018");
-  pinMode(PIN_SENSOR_LEFT, INPUT_PULLUP);
-  pinMode(PIN_SENSOR_F_LEFT, INPUT_PULLUP);
-  pinMode(PIN_SENSOR_FRONT, INPUT_PULLUP);
-  pinMode(PIN_SENSOR_F_RIGHT, INPUT_PULLUP);
-  pinMode(PIN_SENSOR_RIGHT, INPUT_PULLUP);
 
-  pinMode(PIN_LINE_F_LEFT, INPUT);
-  pinMode(PIN_LINE_F_RIGHT, INPUT);
-  pinMode(PIN_LINE_B_LEFT, INPUT);
-  pinMode(PIN_LINE_B_RIGHT, INPUT);
+    SWSerial.begin(9600);
+    ST.autobaud();
 
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW);
+    #if SERIAL > NONE
+        Serial.begin(115200);
+        Serial.println("Init Sumo Robot - CoR 2019");
+    #endif
 
-  Serial.println("Stay for Buton Start ...");
+    pinMode(PIN_SENSOR_LEFT, INPUT_PULLUP);
+    pinMode(PIN_SENSOR_F_LEFT, INPUT_PULLUP);
+    pinMode(PIN_SENSOR_FRONT, INPUT_PULLUP);
+    pinMode(PIN_SENSOR_F_RIGHT, INPUT_PULLUP);
+    pinMode(PIN_SENSOR_RIGHT, INPUT_PULLUP);
 
-  pinMode(PIN_DIP_SWITCH, INPUT_PULLUP);
-  tactic = digitalRead(PIN_DIP_SWITCH);
-  pinMode(PIN_START_BUTTON, INPUT_PULLUP);
-  while(digitalRead(PIN_START_BUTTON));
+    pinMode(PIN_LINE_F_LEFT, INPUT);
+    pinMode(PIN_LINE_F_RIGHT, INPUT);
+    pinMode(PIN_LINE_B_LEFT, INPUT);
+    pinMode(PIN_LINE_B_RIGHT, INPUT);
 
-  digitalWrite(PIN_LED, HIGH);
+    pinMode(PIN_LED, OUTPUT);
+    //digitalWrite(PIN_LED, LOW);
 
-  Serial.println("Stay for 5 seconds ... ");
-  delay(5100);
-  Serial.println("Fight!!!");
+    pinMode(PIN_DIP_SWITCH, INPUT_PULLUP);
+    tactic = digitalRead(PIN_DIP_SWITCH);
+
+    #if SERIAL > NONE
+        Serial.println("Wait for Button Start ...");
+    #endif
+
+    pinMode(PIN_START_BUTTON, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_START_BUTTON), stop, FALLING);
+    while(!digitalRead(PIN_START_BUTTON));
+    digitalWrite(PIN_LED, HIGH);
+
+    #if SERIAL > NONE
+        Serial.println("Wait for 5 seconds ... ");
+    #endif
+
+    delay(5050);
+
+    #if SERIAL > NONE
+        Serial.println("Fight!!!");
+    #endif
+    ST.motor(1, 255);
+    ST.motor(2, 255);
+    delay(250);
+    ST.motor(1, 0);
+    ST.motor(2, 0);
 }
 
 void loop(){
-  count++; // comentar linea para codigo final
-
-  if(measureLine()){
-    survive(); 
-  }
-  searchFWD();
+    /*
+    count++; // Comment out this line for the final code
   
-  if(measureEnemy()){
-    if(searchPID()){
-      fight();
+    if(measureLine()){
+        survive(); 
     }
-  }
+    searchFWD();
+    */
+    
+    if(measureEnemy()){
+        if(searchPID()){
+            fight();
+        }
+    }
+    else if(measureLine()){
+        survive(); 
+    }
+
 /*
   #if DEBUG > NONE
     Serial.print("\nCount Test:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\t");
@@ -160,4 +190,15 @@ void loop(){
     ST.motor(2, 0);
     while(1);
   }*/ 
+}
+
+
+void stop(){ 
+    ST.motor(1, 0);
+    ST.motor(2, 0);
+    digitalWrite(PIN_LED, LOW);
+    while(true){
+        ST.motor(1, 0);
+        ST.motor(2, 0);
+    }
 }
