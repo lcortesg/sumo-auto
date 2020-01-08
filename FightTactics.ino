090a8eb098fee00dbaeac037d5e63f3b78aa23ee
Lucas Cortés <lcortes@alumnos.inf.utfsm.cl>
2020-01-07 22:39:10 -0300

    commit

--- FightTactics.ino
+++ FightTactics.ino
@@ -3,40 +3,43 @@
  */
 bool searchPID(){
     measure = ((enemy_sensor_left + enemy_sensor_f_left + enemy_sensor_front + enemy_sensor_f_right + enemy_sensor_right) == 0)?(measure):(enemy_sensor_left*1000 + enemy_sensor_f_left*2000 + enemy_sensor_front*3000 + enemy_sensor_f_right*4000 + enemy_sensor_right*5000)/(enemy_sensor_left + enemy_sensor_f_left + enemy_sensor_front + enemy_sensor_f_right + enemy_sensor_right);
+
+    error = ref - measure;
+ 
+    error_int += (actuation >= POWER_MAX)?(0):(error);
+
+    error_dev = error - error_prev;
+    
+    error_prev = error;
+    
+    actuation = (error*KP + error_int*KI + error_dev*KD);
+    actuation = (actuation >= POWER_MAX)?(POWER_MAX):(actuation <= -POWER_MAX)?(-POWER_MAX):(actuation);
+    
+    if(abs(actuation) > 50){
+        ST.motor(1, -actuation);
+        ST.motor(2, actuation);
+    }
+
     #if DEBUG > NONE
         Serial.print("\n>>> LOG: measure:\t");
         Serial.println(measure);
-    #endif
-        error = ref - measure;
-    #if DEBUG > NONE
+        
         Serial.print("\n>>> LOG: error:\t");
         Serial.println(error);
-    #endif  
-        error_int += (actuation >= POWER_MAX)?(0):(error);
-    #if DEBUG > NONE
+
         Serial.print("\n>>> LOG: error int:\t");
         Serial.println(error_int);
-    #endif  
-        error_dev = error - error_prev;
-    #if DEBUG > NONE
+
         Serial.print("\n>>> LOG: error_dev:\t");
         Serial.println(error_dev);
-    #endif  
-        error_prev = error;
-    #if DEBUG > NONE
+
         Serial.print("\n>>> LOG: error_prev:\t");
         Serial.println(error_prev);
-    #endif
-        actuation = (error*KP + error_int*KI + error_dev*KD);
-        actuation = (actuation >= POWER_MAX)?(POWER_MAX):(actuation <= -POWER_MAX)?(-POWER_MAX):(actuation);
-    #if DEBUG > NONE
+
         Serial.print("\n>>> LOG: actuation:\t");
         Serial.println(actuation);
     #endif
-        if(abs(actuation) > 50){
-            ST.motor(1, -actuation);
-            ST.motor(2, actuation);
-        }
+
     return ( (enemy_sensor_front == 1) && (abs(actuation) < 50) );
 }
 
@@ -55,9 +58,6 @@ void fight(){
 }
 
 void fight1(){
-    ST.motor(1, POWER_MAX/1);
-    ST.motor(2, POWER_MAX/1);
-    /*
     unsigned long next = millis() + 10000;
     actuation = 0;
     #if DEBUG > 0
@@ -83,7 +83,12 @@ void fight1(){
         ST.motor(1, POWER_MAX);
         ST.motor(2, POWER_MAX);
     }
-    */
+    return;
+}
+
+void fight2(){
+    ST.motor(1, POWER_MAX/1);
+    ST.motor(2, POWER_MAX/1);
     return;
 }
 
